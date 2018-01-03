b15dd0f5443437ded4e94dadfe5a2e6a1b958a47
Derek Schneider <dschneider.641@gmail.com>
2018-01-03 12:10:45 -0500

    Bump to version 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SignedOverpunch.Mixfile do
   def project do
     [
       app: :signed_overpunch,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
